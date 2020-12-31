e2eb840240349f631b72376026cf1c7eb3219d9f
Alexandro Tapia <alexandrotapiaflores@gmail.com>
2020-12-31 10:25:54 -0500

    update

--- start.sh
+++ start.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-#!/bin/bash
-echo "The script you are running has basename `basename "$0"`, dirname `dirname "$0"`"
-echo "The present working directory is `pwd`"cho $DIR
-
-DIR = $(dirname "$0")
+DIR = $(dirname "$(readlink -f "$0")")
 
 python3 -m pip install -r "$DIR"/ChatServer/requirements.txt
 npm --prefix "$DIR"/TelegramBot/ install "$DIR"/TelegramBot/
