043007aaacdd7f812563b54153d14675bf9ba06c
Alexandro Tapia <alexandrotapiaflores@gmail.com>
2020-12-31 10:15:04 -0500

    update

--- start.sh
+++ start.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# get script directory function
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
 
 python3 -m pip install -r "$DIR"/ChatServer/requirements.txt
 npm --prefix "$DIR"/TelegramBot/ install "$DIR"/TelegramBot/
