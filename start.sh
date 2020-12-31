38f982f1cf7709dbe7c359a16559ece512799653
Alexandro Tapia <alexandrotapiaflores@gmail.com>
2020-12-31 10:20:55 -0500

    update

--- start.sh
+++ start.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-# get script directory function
-SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
-  SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
-DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+function absoluteScriptDirPath {
+    SOURCE = $(if [ -z "${BASH_SOURCE[0]}"]; then echo $1; else echo ${BASH_SOURCE[0]}; fi)
+    while [ -h "$SOURCE" ]; do
+      DIR = $( cd -P $( dirname "$SOURCE") && pwd )
+      SOURCE = $(readlink "$SOURCE")
+      [[ $SOURCE != /* ]] && SOURCE = "$DIR/$SOURCE"
+    done
+    DIR = $( cd -P $( dirname "$SOURCE" ) && pwd )
+    echo $DIR
+}
+
+DIR = $(absoluteScriptDirPath $0)
 
 python3 -m pip install -r "$DIR"/ChatServer/requirements.txt
 npm --prefix "$DIR"/TelegramBot/ install "$DIR"/TelegramBot/
