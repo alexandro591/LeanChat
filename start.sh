a78bfb55f85c44a34000437de370e3d728046324
Alexandro Tapia <alexandrotapiaflores@gmail.com>
2020-12-31 10:09:48 -0500

    update

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 python3 -m pip install -r $(DIR)/ChatServer/requirements.txt
-npm install $(DIR)/TelegramBot/
+npm --prefix $(DIR)/TelegramBot/ install $(DIR)/TelegramBot/
 pm2 start python3 --name "ChatServer" -- $(DIR)/ChatServer/main.py
 pm2 start node --name "Bot" -- $(DIR)/TelegramBot/bot.js
 pm2 startup;
