8deba6fa01e183fc069596376c7eaf31efa810c7
Alexandro Tapia <alexandrotapiaflores@gmail.com>
2020-12-31 08:37:55 -0500

    update

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 python3 -m pip install -r $(pwd)/ChatServer/requirements.txt
-npm install $(pwd)/TelegramBot/bot.js
+npm install $(pwd)/TelegramBot/
 pm2 start python3 --name "ChatServer" -- $(pwd)/ChatServer/main.py
 pm2 start node --name "Bot" -- $(pwd)/TelegramBot/bot.js
