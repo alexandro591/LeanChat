90e3313f6127ab9ed43e50dde8dcbfceb00d81f8
Alexandro Tapia <alexandrotapiaflores@gmail.com>
2021-01-01 16:25:21 -0500

    update

--- start.sh
+++ start.sh
@@ -2,9 +2,7 @@
 
 DIR="$(dirname "$(readlink -f "$0")")"
 
-python3 -m pip install -r "$DIR"/ChatServer/requirements.txt
-npm --prefix "$DIR"/TelegramBot/ install "$DIR"/TelegramBot/
-pm2 start python3 --name "ChatServer" -- "$DIR"/ChatServer/main.py
-pm2 start node --name "Bot" -- "$DIR"/TelegramBot/bot.js
+npm --prefix "$DIR"/Server/ install "$DIR"/Server/
+pm2 start node --name "LeanChatServer" -- "$DIR"/Server/main.js
 pm2 startup;
 pm2 save;
--- stop.sh
+++ stop.sh
@@ -1,7 +1,4 @@
-pm2 stop "ChatServer";
-pm2 stop "Bot";
-pm2 flush "ChatServer"
-pm2 flush "Bot"
-pm2 delete "ChatServer";
-pm2 delete "Bot";
+pm2 stop "LeanChatServer";
+pm2 flush "LeanChatServer"
+pm2 delete "LeanChatServer";
 pm2 save --force;
