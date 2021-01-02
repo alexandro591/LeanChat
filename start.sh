9e311dd23aa4bdd0b082283e3a3e90a204b57811
Alexandro Tapia <alexandrotapiaflores@gmail.com>
2021-01-02 03:32:59 -0500

    update

--- start.sh
+++ start.sh
@@ -5,6 +5,6 @@ DIR="$(dirname "$(readlink -f "$0")")"
 npm --prefix "$DIR"/Server/ install "$DIR"/Server/
 npm --prefix "$DIR"/ChatApp/ install "$DIR"/ChatApp/
 pm2 start node --name "LeanChatServer" -- "$DIR"/Server/main.js
-pm2 start npm --name "LeanChatApp" -- start -- prefix "$DIR"/ChatApp
+pm2 start npm --name "LeanChatApp" -- start --prefix "$DIR"/ChatApp
 pm2 startup
 pm2 save
