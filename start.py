623a3c4e2b734145b09de602574742d4f0405734
Alexandro Tapia <alexandrotapiaflores@gmail.com>
2021-01-04 00:26:38 -0500

    update

--- start.py
+++ start.py
@@ -2,9 +2,8 @@ import os
 
 DIR = os.path.dirname(os.path.realpath(__file__))
 
-os.system(f"npm --prefix \"{DIR}/Server/\" install \"{DIR}/Server/\"")
-os.system(f"npm --prefix \"{DIR}/Server/\" install \"{DIR}/Server/\"")
-os.system(f"npm --prefix \"{DIR}/ChatApp/\" install \"{DIR}/ChatApp/\"")
+os.system(f"npm install --prefix \"{DIR}/Server/\" \"{DIR}/Server/\"")
+os.system(f"npm install --prefix \"{DIR}/ChatApp/\" \"{DIR}/ChatApp/\"")
 os.system(f"pm2 start node --name \"LeanChatServer\" -- \"{DIR}/Server/main.js\"")
 os.system(f"pm2 start npm --name \"LeanChatApp\" -- start --prefix \"{DIR}/ChatApp\"")
 os.system("pm2 startup")
