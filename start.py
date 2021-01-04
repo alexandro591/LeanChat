7fdcec47b497e454c6473323a120ff83da74b7ef
Alexandro Tapia <alexandrotapiaflores@gmail.com>
2021-01-04 00:34:53 -0500

    UPDATE

--- start.py
+++ start.py
@@ -1,10 +1,18 @@
 import os
 
 DIR = os.path.dirname(os.path.realpath(__file__))
+OS_NAME = os.name
+COMMANDS = [
+    f"npm install --prefix \"{DIR}/Server/\" \"{DIR}/Server/\"",
+    f"npm install --prefix \"{DIR}/ChatApp/\" \"{DIR}/ChatApp/\"",
+    f"pm2 start node --name \"LeanChatServer\" -- \"{DIR}/Server/main.js\"",
+    f"pm2 start npm --name \"LeanChatApp\" -- start --prefix \"{DIR}/ChatApp\"",
+    "pm2 startup",
+    "pm2 save"
+]
 
-os.system(f"npm install --prefix \"{DIR}/Server/\" \"{DIR}/Server/\"")
-os.system(f"npm install --prefix \"{DIR}/ChatApp/\" \"{DIR}/ChatApp/\"")
-os.system(f"pm2 start node --name \"LeanChatServer\" -- \"{DIR}/Server/main.js\"")
-os.system(f"pm2 start npm --name \"LeanChatApp\" -- start --prefix \"{DIR}/ChatApp\"")
-os.system("pm2 startup")
-os.system("pm2 save")
+for command in COMMANDS:
+    if OS_NAME == "nt":
+        os.system(command.replace("/", "\\"))
+    else:
+        os.system(command)
