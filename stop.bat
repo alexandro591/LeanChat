ba99f9f555ed7878c39a4e5d86bcf62ac3f19054
Alexandro Tapia <alexandrotapiaflores@gmail.com>
2021-01-04 23:15:27 -0500

    update

--- stop.bat
+++
@@ -1,8 +0,0 @@
-call pm2 stop "LeanChatServer"
-call pm2 flush "LeanChatServer"
-call pm2 delete "LeanChatServer"
-call pm2 stop "LeanChatApp"
-call pm2 flush "LeanChatApp"
-call pm2 delete "LeanChatApp"
-
-call pm2 save --force
