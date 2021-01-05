6cc9732eebe10f3f246f297a43d16859ebeea002
Alexandro Tapia <alexandrotapiaflores@gmail.com>
2021-01-04 20:15:30 -0500

    update

--- ChatApp/app.js
+++ ChatApp/app.js
@@ -15,11 +15,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public'),{
+app.use('/chat/*/init', express.static(path.join(__dirname, './public/app/build'),{
   extensions: ['html', 'htm'],
 }));
 
-app.use('/chat', chat);
+app.use('/chiat', chat);
 
 app.use(function(req, res, next) {
   next(createError(404));
